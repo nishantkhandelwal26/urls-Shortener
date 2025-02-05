f45a372d6d65bffbda1e568c578938b7e524398d
nishantkhandelwal26 <165279743+nishantkhandelwal26@users.noreply.github.com>
2025-02-05 18:04:03 +0530

    Create JwtAuthenticationFilter.java

---
+++ src/main/java/com/url/shortener/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.url.shortener.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// Spring security recognizes JwtAuthenticationFilter as a filter that will be executed once per request
+// By default Spring security doesn't automatically include this custom filter in the filter chain unless we explicitly add it
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtTokenProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            // Get JWT from header
+            // Validate Token
+            // If valid get user details
+            String jwt = jwtTokenProvider.getJwtFromHeader(request);
+            if(jwt!=null && jwtTokenProvider.validateToken(jwt)){
+                String username = jwtTokenProvider.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if(userDetails!=null){
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
